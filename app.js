640d2d06261b65dc0acc93cbddbc1f07de1fc09c
tanay306 <tanaygandhi306@GMAIL.COM>
2020-09-10 01:06:48 +0530

    edited projects code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+// const path = require("path");
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
@@ -10,13 +10,13 @@ mongoose.set('useFindAndModify', false);
 
 const employeeRouter = require("./router/employeeRouter");
 const customerRouter = require("./router/customerRouter");
-// const projectRouter = require("./router/projectRouter");
+const projectRouter = require("./router/projectRouter");
 
 ////////////////////----- Routers -----////////////////////
 
 app.use("/employee", employeeRouter);
 app.use("/customer", customerRouter);
-// app.use("/project", projectRouter);
+app.use("/project", projectRouter);
 
 
 ////////////////////----- Server has started message -----////////////////////
